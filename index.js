3b74e6456172d126940da40c553af8dc10daa560
Adrian Lynch <adi_ady_ade@hotmail.com>
2016-02-03 08:13:03 +0000

    Add API documentation

--- index.js
+++ index.js
@@ -9,16 +9,43 @@ server.connection({
 	port: 8000
 })
 
+server.register(
+	[
+		require('inert'),
+		require('vision'),
+		{
+			register: require('hapi-swagger'),
+			options: {
+				info: {
+					'title': 'API Documentation - Swagger'
+				}
+			}
+		}
+	],
+	(err) => {
+
+	if (err) {
+		console.log('Failed to register one or more plugins');
+		throw err;
+	}
+
+	server.start(err => {
+
+		if (err) {
+			console.log('Failed to start the server')
+			throw err
+		}
+
+		console.log('Server running at:', server.info.uri)
+
+	})
+})
+
 server.route({
 	method: 'get',
 	path: '/',
-	handler: (request, reply) => reply({up: "and", running: "!"})
-})
-
-server.start(err => {
-	if (err) {
-		console.log('Failed to start the server')
-		throw err
+	config: {
+		handler: (request, reply) => reply({up: "and", running: "!"}),
+		tags: ['api']
 	}
-	console.log('Server running at:', server.info.uri)
 })
