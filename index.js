939c2d6ad20d38e878f47075a42feb971ce1f267
Adrian Lynch <adi_ady_ade@hotmail.com>
2016-02-03 09:22:59 +0000

    Add Mongo connection and prep for users

--- index.js
+++ index.js
@@ -20,13 +20,19 @@ server.register(
 					'title': 'API Documentation - Swagger'
 				}
 			}
+		},
+		{
+			register: require('hapi-mongodb'),
+			options: {
+				url: 'mongodb://localhost:27017/everything'
+			}
 		}
 	],
 	(err) => {
 
 	if (err) {
-		console.log('Failed to register one or more plugins');
-		throw err;
+		console.log('Failed to register one or more plugins')
+		throw err
 	}
 
 	server.start(err => {
@@ -41,11 +47,25 @@ server.register(
 	})
 })
 
-server.route({
-	method: 'get',
-	path: '/',
-	config: {
-		handler: (request, reply) => reply({up: "and", running: "!"}),
-		tags: ['api']
+// Temp routes for now
+server.route([
+	{
+		method: 'get',
+		path: '/',
+		config: {
+			handler: (request, reply) => reply({up: "and", running: "!"}),
+			tags: ['api']
+		}
+	},
+	{
+		method: 'get',
+		path: '/load',
+		config: {
+			handler: (request, reply) => {
+				const db = request.server.plugins['hapi-mongodb'].db
+				const col = db.collection('users');
+				reply(col.find({}).toArray())
+			}
+		}
 	}
-})
+])
