8ca2265908650122179c619c724f5a95076e81ec
Adrian Lynch <adi_ady_ade@hotmail.com>
2016-02-04 11:06:51 +0000

    Add CrUd for user

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 'use strict'
 
 const Hapi = require('hapi')
+const Joi = require('joi')
+const Boom = require('boom')
 
 const server = new Hapi.Server()
 
@@ -54,28 +56,58 @@ server.route([
 		method: 'get',
 		path: '/users',
 		config: {
-			handler: (request, reply) => reply([{}, {}])
+			handler: (request, reply) => {
+				const col = request.server.plugins['hapi-mongodb'].db.collection('users')
+				return reply(col.find({}).limit(10).toArray())
+			},
+			tags: ['api']
 		}
 	},
 	{
 		method: 'post',
 		path: '/users',
 		config: {
-			handler: (request, reply) => reply([{}, {}]).code(201)
+			handler: (request, reply) => {
+				const col = request.server.plugins['hapi-mongodb'].db.collection('users')
+				col.insert(request.payload, (err, results) => {
+					if (err) {
+						return reply(err)
+					}
+					reply(results.ops.map(item => {
+						return {
+							id: item.id,
+							firstName: item.firstName,
+							lastName: item.lastName,
+							email: item.email
+						}
+					})).code(201)
+				})
+			},
+			validate: {
+				payload: {
+					id: Joi.string().required(),
+					firstName: Joi.string().required(),
+					lastName: Joi.string().required(),
+					email: Joi.string().email().required()
+				}
+			},
+			tags: ['api']
 		}
 	},
 	{
 		method: 'put',
 		path: '/users/{id}',
 		config: {
-			handler: (request, reply) => reply({})
+			handler: (request, reply) => reply(Boom.notImplemented()),
+			tags: ['api']
 		}
 	},
 	{
 		method: 'delete',
 		path: '/users/{id}',
 		config: {
-			handler: (request, reply) => reply().code(204)
+			handler: (request, reply) => reply(Boom.notImplemented()),
+			tags: ['api']
 		}
 	},
 	{
@@ -83,12 +115,15 @@ server.route([
 		path: '/load',
 		config: {
 			handler: (request, reply) => {
+				// TEMP: This whole route can go once the CRUD for users is in place, for now:
+				return reply(Boom.notImplemented())
 				const db = request.server.plugins['hapi-mongodb'].db
 				const col = db.collection('users')
 				const users = require('./users.json')
 				col.insertMany(users)
-				reply(col.find({}).toArray())
-			}
+				return reply()
+			},
+			tags: ['api']
 		}
 	}
 ])
