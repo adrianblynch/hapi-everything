f549ba23db08e426b1a1eb73ca2355956c18303e
Adrian Lynch <adi_ady_ade@hotmail.com>
2016-05-14 21:39:08 +0100

    Add (the worse possible) tests to find things on first load

--- test/e2e/firstLoad.js
+++ test/e2e/firstLoad.js
@@ -4,4 +4,11 @@ Feature('Page initially loads');
 Scenario('Going to the base URL', (I) => {
 	I.amOnPage('/');
 	I.seeInTitle('Hapi Everything - React MUI');
+	I.see('React Material UI')
+	I.see('DELETE SELECTED USERS');
+	I.see('ADD NEW USER');
+	I.see('ID')
+	I.see('First name')
+	I.see('Last name')
+	I.see('Email')
 });
